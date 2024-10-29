bbd5aa931f5ebb8a0de3256cecf01e0d5c172099
asqrdn <danilm@sibguti.ru>
2024-10-29 12:35:57 +0700

    Fix outline

--- src/components/search/SearchInput.vue
+++ src/components/search/SearchInput.vue
@@ -8,7 +8,7 @@ export default {
   methods: {
     ...mapActions(["changeSearchName"]),
     changeValue() {
-      const changed = this.search_username.split(/[\s,]+/);//Сторка разибвается на массив через запятую и пробел
+      const changed = this.search_username.split(/[\s,]+/);//
       this.changeSearchName(changed);
     },
   },
@@ -36,20 +36,22 @@ $placeholder: rgba(118, 120, 125, 1)
 
 $border: rgba(233, 236, 239, 1)
 
+.search-input
+  height: 46px
+  padding: 0 16px 0 16px
+  border-radius: 8px
+  border: 1.5px solid $border
+  transition: outline .1s ease-in
+  outline: $border 0px solid
+  font-family: var.$font-family
+
+  &::placeholder
+    color: $placeholder
+
+  &:focus
+    outline: $border 3px solid
+
 .search-input-container
   display: grid
-  .search-input
-    height: 46px
-    padding: 0 16px 0 16px
-    border-radius: 8px
-    border: 1.5px solid $border
-    transition: .1s ease-in
-    font-family: var.$font-family
-
-    &::placeholder
-      color: $placeholder
-
-    &:focus
-      outline: $border 3px solid
 </style>
 
